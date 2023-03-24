73b7d3c3ff2226cc77e0e538012fe78c6697d9cb
BarbozaDuda <128854000+BarbozaDuda@users.noreply.github.com>
2023-03-24 18:01:43 -0300

    Add files via upload

---
+++ 1trab.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>3daw</title>
+  </head>
+  <body>
+    <?php 
+    echo "<h2> Exercicio com array </h2>";
+    $carros = array ("fusca","opala", "dodge","corsel");
+    foreach ($carros as $carro )
+    {
+      echo "meu carro é um $carro <br>";
+    }
+    ?> 
+  </body>
+</html>
